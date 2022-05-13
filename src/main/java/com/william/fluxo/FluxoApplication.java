9f491ceab304304425ed3e7cbb3dfb7fdf520b93
Finster <tp.william.f@d0007azurecloudtotvscom.onmicrosoft.com>
2022-05-13 20:16:45 -0300

    Initial commit

---
+++ src/main/java/com/william/fluxo/FluxoApplication.java
@@ -0,0 +1,13 @@
+package com.william.fluxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxoApplication.class, args);
+    }
+
+}
